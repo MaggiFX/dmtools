6780864cc859e875609d8958c5a3ef66bf7b851b
Robert Lang <robert.lang@ion2s.com>
2018-10-08 16:40:36 +0200

    Geldklasse erweitert und Hauptfenster um Reiter ergänzt.

--- Klassen/Systeme/Geld.cs
+++ Klassen/Systeme/Geld.cs
@@ -8,13 +8,27 @@ namespace dmtools
 {
     class Geld
     {
+        public static Geld operator +(Geld a, Geld b)
+        {
+            Geld c = new Geld();
+            c.Kupfer = a.Kupfer + b.Kupfer;
+            c.Silber = a.Silber + b.Silber;
+            c.Gold   = a.Gold   + b.Gold;
+            return c;
+        }
+
         public int Kupfer { get; set; } // 1 Münze = 10 gramm / 0,01 kg => 100 Münzen = 1kg
         public int Silber { get; set; } // 1 Silber = 10 Kupfer
         public int Gold   { get; set; } // 1 Gold = 10 Silber = 100 Kupfer
-
         //public int Elektrum { get; set; } // 1 Elektrum = 5 Silber = 50 Kupfer
         //public int Platin { get; set; }   // 1 Platin = 10 Gold = 100 Silber = 1000 Kupfer
 
+        public void AddGeld(Geld geldin)
+        {
+            Kupfer += geldin.Kupfer;
+            Silber += geldin.Silber;
+            Gold   += geldin.Gold;
+        }
         public void AddGeld(int kupferin)
         {
             Kupfer += kupferin;
@@ -29,6 +43,24 @@ namespace dmtools
             Kupfer += kupferin;
             Silber += silberin;
             Gold   += goldin;
+        }
+
+        public void RemoveGeld(Geld geldin)
+        {
+
+        }
+
+        public void SetGeld(Geld geldin)
+        {
+            Kupfer = geldin.Kupfer;
+            Silber = geldin.Silber;
+            Gold   = geldin.Gold;
+        }
+        public void SetGeld(int goldin, int silberin, int kupferin)
+        {
+            Kupfer = kupferin;
+            Silber = silberin;
+            Gold   = goldin;
         }
 
     }
